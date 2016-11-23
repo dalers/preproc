44ef5ffaf9a1a4d8e24d77391ee5b22fc4403e9b
Dale Scott <dale@dalescott.net>
2016-11-23 03:21:41 -0700

    wip - use getline to read line by line

--- preproc.c
+++ preproc.c
@@ -1,8 +1,7 @@
+#define _WITH_GETLINE
 #include <stdio.h>
-// #include <ctype.h>
-// #include <stdlib.h>
 
-FILE *fp;
+#define MAXLINE 1000          // maximum data file line length
 
 int help()
 {
@@ -18,47 +17,24 @@ int help()
 
 int main()
 {
-  int c;                        // holds char read from data file
+  int len;                      // current line length
+  int max;                      // maximum length seen so far
+  char line[MAXLINE];           // current input line
+
   int sample;                   // holds sample index read from data file
   float bx, by, bz, temp;       // holds sample values read from data file
 
+  ssize_t byteCount;
+
   help();                       // output usage for fun
 
-  while ((c = getchar()) != EOF)
-    putchar(c);
+  while ((byteCount = getline(line, sizeof(line))) > 0) {
+    if (sscanf(line, "%i ; %f ; %f ; %f ; %f", &sample, &bx, &by, &bz, &temp) == 5)
+      printf("valid data record: %s\n", line);
+    else
+      printf("invalid: %s\n", line);
+  }
+
   return(0);
-
-
-/* NEW WAY FROM K&R
-while (getline(line, sizeof(line)) > 0) {
-  if (sscanf(line, "%d %s %d", &day, monthname, &year) == 3)
-    printf("valid: %s\n", line);
-  else if (sscanf(line, "%d/%d/%d", &month, &day, &year) == 3)
-    printf("valid: %s\n", line);
-  else
-    printf("invalid: %s\n", line);
 }
-*/
-
-/* OLD WAY
-  char filename[12] = "test.log";       // holds input data filename
-  int inChar;                           // character read from input data file
-
-  if ((fp=fopen(filename, "r"))==NULL)
-  {
-    printf("\n\n*** data file does not exist ***\n");
-    exit(1);                            // exits program with error condition
-  }
-
-  inChar = getc(fp);                    // reads first character from data file
-  while (inChar != EOF)
-  {
-    putchar(inChar);
-    inChar = getc(fp);
-  }
-  fclose(fp);
-  return(0);
-*/
-
-} 
-
+ 
